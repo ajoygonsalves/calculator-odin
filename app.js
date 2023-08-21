cda61c43468ce75d51e97e6cfb6848768978a458
ajoyous <ajoy.gonsalves@hotmail.com>
2023-08-21 22:16:34 +0200

    add operate function which called respective basic arithmetic function

--- app.js
+++ app.js
@@ -13,3 +13,25 @@ const multiply = function (a, b) {
 const divide = function (a, b) {
   return a / b;
 };
+
+let firstNum;
+let secondNum;
+let operation;
+
+const operate = function (a, b, op) {
+  switch (op) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+      break;
+    case "/":
+      return divide(a, b);
+    default:
+      return "ERROR";
+  }
+};
+
+console.log(operate(1, 2, "+"));
